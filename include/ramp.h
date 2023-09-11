3bdfce664c70e5b69473f35d1c1a066990bc0c65
Carlos Kometter <ckometter@gmail.com>
2023-09-11 12:10:50 -0700

    Deleting undefined buffer function

--- include/ramp.h
+++ include/ramp.h
@@ -25,7 +25,6 @@ private:
 
 public:
 	uint8_t rampCmd[20];
-	uint8_t buffer(void);
 	uint8_t simpleRamp(uint8_t channelsDAC[4], double vi[4], double vf[4], double nSteps, double del, bool buffer);
 	uint8_t simpleRampIteration(uint8_t channelsDAC[4], double vi[4], double nSteps, double del);
 	uint8_t bufferRampIteration(uint8_t channelsDAC[4], double vi[4], double nSteps, double del);
